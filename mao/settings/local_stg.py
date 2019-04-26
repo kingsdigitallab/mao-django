360939ba8eded0e31bfeebd5e773f52a717a8304
Brian Maher <brian.maher@kcl.ac.uk>
2019-04-26 07:58:49 +0000

    Fixing redis url

--- mao/settings/local_stg.py
+++ mao/settings/local_stg.py
@@ -3,7 +3,7 @@ from .base import *  # noqa
 ALLOWED_HOSTS = ['mao-stg.kdl.kcl.ac.uk']
 
 CACHE_REDIS_DATABASE = '1'
-CACHES['default']['LOCATION'] = '127.0.0.1:6379:' + CACHE_REDIS_DATABASE
+CACHES['default']['LOCATION'] = 'redis://127.0.0.1:6379/' + CACHE_REDIS_DATABASE
 
 INTERNAL_IPS = INTERNAL_IPS + ['']
 ALLOWED_HOSTS = ['']
