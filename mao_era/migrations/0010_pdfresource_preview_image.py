0597159ef374686007a2c2705104dc3ab178a827
Jamie Norrish <jamie@artefact.org.nz>
2019-06-05 06:13:14 +0000

    Changed migration to use default value of an existing image on stg.

--- mao_era/migrations/0010_pdfresource_preview_image.py
+++ mao_era/migrations/0010_pdfresource_preview_image.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='pdfresource',
             name='preview_image',
-            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.PROTECT, related_name='pdfs', to='wagtailimages.Image'),
+            field=models.ForeignKey(default=22, on_delete=django.db.models.deletion.PROTECT, related_name='pdfs', to='wagtailimages.Image'),
             preserve_default=False,
         ),
     ]
