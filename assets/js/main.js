58d560360025cd9ccf97d60754dae416a23b8fe4
olgaloboda <olga.loboda.13@gmail.com>
2019-07-30 16:01:00 +0100

    fixed autoplay for videos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,8 @@
 $(function() {
     // Your JS Here
+    $('#content1 a').click(function(event) {
+        $('.name-active').removeClass('name-active'); 
+        $target = $(event.currentTarget); 
+        $target.addClass('name-active');
+    });
 });
